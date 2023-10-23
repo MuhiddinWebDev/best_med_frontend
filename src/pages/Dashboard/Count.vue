f028515b12d4767e5edcb0f4d62d4447da306d89
Nurmuhammad <nurmuhammadnizomov13@gmail.com>
2023-10-23 14:08:07 +0500

    count

--- src/pages/Dashboard/Count.vue
+++ src/pages/Dashboard/Count.vue
@@ -11,127 +11,83 @@
 
 <script>
 import VueApexCharts from "vue-apexcharts";
-import axios from "axios";
+import axios from 'axios';
 export default {
-  name: "test",
   components: {
     apexchart: VueApexCharts,
   },
   data() {
     return {
-      series: [
-        {
-          name: "Кўриклар сони",
-          data: [],
-        },
-      ],
+      series: [],
       chartOptions: {
         chart: {
-          height: 350,
           type: "bar",
+          height: 350,
         },
         plotOptions: {
           bar: {
-            borderRadius: 10,
-            dataLabels: {
-              position: "top", 
-            },
+            horizontal: false,
+            columnWidth: "55%",
+            endingShape: "rounded",
           },
         },
         dataLabels: {
-          enabled: true,
-          formatter: function(val) {
-            return val;
-          },
-          offsetY: -20,
-          style: {
-            fontSize: "12px",
-            colors: ["#304758"],
-          },
+          enabled: false,
         },
-
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ["transparent"],
+        },
         xaxis: {
           categories: [
-              "Январ",
-              "Феврал",
-              "Март",
-              "Апрел",
-              "Май",
-              "Июн",
-              "Июл",
-              "Август",
-              "Сентябр",
-              "Октябр",
-              "Ноябр",
-              "Декабр"
+            "Январ",
+            "Феврал",
+            "Март",
+            "Апрел",
+            "Май",
+            "Июн",
+            "Июл",
+            "Август",
+            "Сентябр",
+            "Октябр",
+            "Ноябр",
+            "Декабр"
           ],
-          position: "top",
-          axisBorder: {
-            show: false,
-          },
-          axisTicks: {
-            show: false,
-          },
-          crosshairs: {
-            fill: {
-              type: "gradient",
-              gradient: {
-                colorFrom: "#D8E3F0",
-                colorTo: "#BED1E6",
-                stops: [0, 100],
-                opacityFrom: 0.4,
-                opacityTo: 0.5,
-              },
-            },
-          },
-          tooltip: {
-            enabled: true,
-          },
         },
         yaxis: {
-          axisBorder: {
-            show: false,
+          title: {
+            text: "Кўриклар сони",
           },
-          axisTicks: {
-            show: false,
-          },
-          labels: {
-            show: false,
+        },
+        fill: {
+          opacity: 1,
+        },
+        tooltip: {
+          y: {
             formatter: function(val) {
               return val;
             },
           },
         },
-        title: {
-          text: "",
-          floating: true,
-          offsetY: 330,
-          align: "center",
-          style: {
-            color: "#444",
-          },
-        },
       },
     };
   },
+  methods: {
+    async get() {
+      try {
+        let res = await axios.post("/hisobot/tekshiruvcount")
+        console.log(res.data)
+        if(res.data) {
+          this.series = res.data
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
   mounted() {
     this.get()
-  },
-  methods: {
-    async get () {
-      try {
-        let res = await axios.post("/hisobot/tekshirushcount")
-        if(res.data) {
-          this.series[0].data = res.data
-        }
-        
-        setTimeout(() => {
-          this.get();
-        }, 5000);
-      } catch (error) {
-        console.error(error)
-      }
-    } 
   }
 };
 </script>
